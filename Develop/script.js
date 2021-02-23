6ecde0bdae169975092b7df42ff9a72920350048
Erin Seppala <aseppala98@gmail.com>
2021-02-23 15:19:38 -0500

    Added Current Day function to html

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,14 @@
-var DateTime = luxon.DateTime;
+// header shows current day of the week and month/day
+// text blocks are changed dynamically based on time (past, present, future)
+// when clicking on a timeblock, can enter an event
+// when clicking on the save button for that timeblock, the text is saved to local storage
+// when refreshing the page, the saved events persist
+
+// variables
+
+let DateTime = luxon.DateTime;
+
+let now = DateTime.now();
+
+// runtime
+$("#currentDay").text(now.toLocaleString(DateTime.DATETIME_MED));
