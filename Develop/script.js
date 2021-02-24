b7cc22d41ad1447dabaad06158c24639eaf2bf38
Erin Seppala <aseppala98@gmail.com>
2021-02-24 11:39:12 -0500

    Added checkTime functionality

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,39 @@
 // when clicking on the save button for that timeblock, the text is saved to local storage
 // when refreshing the page, the saved events persist
 
-// variables
-
+        // <!--variables-->
+// Luxon Variables
 let DateTime = luxon.DateTime;
-
 let now = DateTime.now();
 
-// runtime
+// Sets Current Date/Time to Header
+let currentTime = moment().hour(); 
 $("#currentDay").text(now.toLocaleString(DateTime.DATETIME_MED));
+
+let saveBtn = $(".saveBtn");
+let textarea = $(".description");
+
+
+        // <!--functions-->
+
+// function that changes timeblock colors based on current time
+function checkTime() {
+  for (let i = 0; i < textarea.length; i++) {
+    if (textarea[i].getAttribute("value") > currentTime) {
+      textarea[i].classList.add("future");
+    } else if (textarea[i].getAttribute("value") < currentTime) {
+      textarea[i].classList.add("past");
+    } else {
+      textarea[i].classList.add("present");
+    }
+  }
+}        
+
+
+        // <!--runtime-->
+
+// run on page load
+function init() {
+  checkTime();
+}
+$(document).ready(init());
